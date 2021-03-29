2a3d760f2b7abffa3b521b7e2cc74f297c94ad6e
Warrenayiss <warrenayiss@outlook.com>
2021-03-29 23:44:09 +0000

    getting the date into text on the app

--- app/src/main/java/com/example/ageinminute/MainActivity.kt
+++ app/src/main/java/com/example/ageinminute/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.ageinminute
 
+
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,8 +31,15 @@ class MainActivity : AppCompatActivity() {
         DatePickerDialog(
             this,
             DatePickerDialog.OnDateSetListener {
-                    view, year, month, dayOfMonth ->
-                Toast.makeText(this, "DatePicker works", Toast.LENGTH_LONG).show()
+                    view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this, "The chosen year is" + selectedYear, Toast.LENGTH_LONG).show()
+
+                val selectedDate:String = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                tvSelectedDate.setText(selectedDate)
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                val theDate = sdf.parse(selectedDate)
             }, year,
             month,
             dayOfMonth).show()
